0684b96f707c2083dc32e94694a05af802085986
Ante Adamovic <ante.adamovic93@gmail.com>
2016-06-22 11:35:43 +0200

    Initialize VILS.java

---
+++ VeryIntelligentLanguageSystem.java
@@ -0,0 +1,9 @@
+public class VeryIntelligentLanguageSystem {
+    private static String fullName = "Very Intelligent Language System";
+    private static String name = "V.I.L.S.";
+    private static String shortName = "VILS";
+
+    public static void main(String[] args){
+	System.out.println("Hello, my name is " + name + " how can I be of service.");
+    }
+}
